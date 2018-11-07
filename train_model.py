335b7e405d804c4392eca4e5bb6ccdb306405c8f
craigdunton <craigodawg19@gmail.com>
2018-11-07 12:57:52 -0500

    added visualization of training

--- train_model.py
+++ train_model.py
@@ -140,7 +140,7 @@ def cnn_model_fn(features, labels, mode):
     print("36 neuron logit layer")
     # 26 softmax (may need to be 36 for numbers too)
     # use softmax activation here?
-    logits = tf.layers.dense(inputs=dense2, units=32)
+    logits = tf.layers.dense(inputs=dense2, units=32, name="logits")
 
     predictions = {
         # Generate predictions (for PREDICT and EVAL mode)
@@ -149,6 +149,9 @@ def cnn_model_fn(features, labels, mode):
         # `logging_hook`.
         "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
     }
+
+    if mode == "VISUALIZE":
+        return logits
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
@@ -171,31 +174,118 @@ def cnn_model_fn(features, labels, mode):
     return tf.estimator.EstimatorSpec(
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-classifier = tf.estimator.Estimator(
-    model_fn=cnn_model_fn, model_dir="./tmp/cnn_model")
+# classifier = tf.estimator.Estimator(
+#     model_fn=cnn_model_fn, model_dir="./tmp/cnn_model")
 
-# Set up logging for predictions
-tensors_to_log = {"probabilities": "softmax_tensor"}
-logging_hook = tf.train.LoggingTensorHook(
-    tensors=tensors_to_log, every_n_iter=50)
 
-# Train the model
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": train_data},
-    y=train_labels,
-    batch_size=1,
-    num_epochs=None,
-    shuffle=True)
-classifier.train(
-    input_fn=train_input_fn,
-    steps=20000,
-    hooks=[logging_hook])
+# # Set up logging for predictions
+# tensors_to_log = {"probabilities": "softmax_tensor"}
+# logging_hook = tf.train.LoggingTensorHook(
+#     tensors=tensors_to_log, every_n_iter=50)
+
+# # Train the model
+# train_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     x={"x": train_data},
+#     y=train_labels,
+#     batch_size=1,
+#     num_epochs=None,
+#     shuffle=True)
+
+logits = cnn_model_fn({"x": train_data}, train_labels, "VISUALIZE")
+
+# create summaries for visualization
+with tf.name_scope('cross_entropy'):
+    # The raw formulation of cross-entropy,
+    #
+    # tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.softmax(y)),
+    #                               reduction_indices=[1]))
+    #
+    # can be numerically unstable.
+    #
+    # So here we use tf.losses.sparse_softmax_cross_entropy on the
+    # raw logit outputs of the nn_layer above, and then average across
+    # the batch.
+    with tf.name_scope('total'):
+        cross_entropy = tf.losses.sparse_softmax_cross_entropy(
+            labels=train_labels, logits=logits)
+tf.summary.scalar('cross_entropy', cross_entropy)
+
+with tf.name_scope('train'):
+    learning_rate = .01
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(
+        cross_entropy)
+
+    
+with tf.name_scope('accuracy'):
+    with tf.name_scope('correct_prediction'):
+        correct_prediction = tf.equal(tf.argmax(logits, 1), train_labels)
+    with tf.name_scope('accuracy'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+tf.summary.scalar('accuracy', accuracy)
+
+# set up for running the session
+sess = tf.InteractiveSession()
+
+# Input placeholders
+with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, [29058, 20, 20], name='x-input')
+    y_ = tf.placeholder(tf.int64, [None], name='y-input')
+    keep_prob = tf.placeholder(tf.float32)
+
+feed_dict = {x: train_data, y_: train_labels, keep_prob: 1.0}
+steps = 250 # was 2000
+
+# merge summaries
+merged = tf.summary.merge_all()
+train_writer = tf.summary.FileWriter("logs/train", sess.graph)
+test_writer = tf.summary.FileWriter("logs/test") #shouldn't need this
+tf.global_variables_initializer().run()
+
+for i in range(steps):
+    if i % 10 == 0:  # Record summaries and test-set accuracy
+      summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict)
+      test_writer.add_summary(summary, i)
+      print('Accuracy at step %s: %s' % (i, acc))
+    else:  # Record train set summaries, and train
+      if i % 100 == 99:  # Record execution stats
+        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        run_metadata = tf.RunMetadata()
+        summary, _ = sess.run([merged, train_step],
+                              feed_dict=feed_dict,
+                              options=run_options,
+                              run_metadata=run_metadata)
+        train_writer.add_run_metadata(run_metadata, 'step%d' % i)
+        train_writer.add_summary(summary, i)
+        print('Adding run metadata for', i)
+      else:  # Record a summary
+        summary, _ = sess.run([merged, train_step], feed_dict=feed_dict)
+        train_writer.add_summary(summary, i)
+
+train_writer.close()
+test_writer.close()
+
+# classifier.train(
+#     input_fn=train_input_fn,
+#     steps=2000,
+#     hooks=[logging_hook])
 
 # Evaluate the model and print results
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": eval_data},
-    y=eval_labels,
-    num_epochs=1,
-    shuffle=False)
-eval_results = classifier.evaluate(input_fn=eval_input_fn)
-print(eval_results)
+# eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     x={"x": eval_data},
+#     y=eval_labels,
+#     num_epochs=1,
+#     shuffle=False)
+# eval_results = classifier.evaluate(input_fn=eval_input_fn)
+# print(eval_results)
+
+# predict from eval_data and eval_labels
+# predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     x={"x": np.array([eval_data[0]])},
+#     num_epochs=1,
+#     shuffle=False
+# )
+
+# print("LABELS: ", eval_labels[0])
+# the_predictions = classifier.predict(input_fn=predict_input_fn)
+# for p in the_predictions:
+#     print(p)
